088558d81907815907ab70b130573c6a88863979
Maxim <flaren265@yandex.ru>
2022-09-01 00:31:03 +0300

    задача 2

---
+++ задача 2/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите два числа.");
+Console.Write("Первое число: ");
+int num1 = int.Parse(Console.ReadLine());
+Console.Write("Второе число: ");
+int num2 = int.Parse(Console.ReadLine());
+if (num1 > num2)
+{
+    Console.WriteLine(num1 + " большее");
+    Console.Write(num2 + " меньшее");
+}
+else if (num1 == num2)
+{
+    Console.WriteLine(num1 + " равно " + num2);
+}
+else
+{
+    Console.WriteLine(num2 + " большее");
+    Console.Write(num1 + " меньшее");
+}
